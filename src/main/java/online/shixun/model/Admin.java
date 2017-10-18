d0b37167212e6a4da05f4dd7029ebb4c67c45977
huang,yong <huangyong.nsu@qq.com>
2017-10-18 16:22:28 +0800

    建库

---
+++ src/main/java/online/shixun/model/Admin.java
@@ -0,0 +1,20 @@
+package online.shixun.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_admin")
+public class Admin {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long adminId;	
+	@Column(unique=true)
+	private String adminName;	
+	@Column(unique=true)
+	private String AdminPassword;
+}
