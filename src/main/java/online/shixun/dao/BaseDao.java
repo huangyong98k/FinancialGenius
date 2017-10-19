e4c80fdc1667f7d22fe49aeaf0e34b45b281606f
huang,yong <huangyong.nsu@qq.com>
2017-10-19 11:29:44 +0800

    updata BaseDao.java

--- src/main/java/online/shixun/dao/BaseDao.java
+++ src/main/java/online/shixun/dao/BaseDao.java
@@ -8,6 +8,9 @@
 */
 package online.shixun.dao;
 
+import java.util.List;
+
+import org.springframework.orm.hibernate4.HibernateTemplate;
 import org.springframework.orm.hibernate4.support.HibernateDaoSupport;
 
 /** 
@@ -17,7 +20,14 @@ import org.springframework.orm.hibernate4.support.HibernateDaoSupport;
 * @date 2017年10月19日 上午10:56:45 
 *  
 */
-public abstract class BaseDao extends HibernateDaoSupport{
-	
+public abstract class BaseDao<E> extends HibernateDaoSupport{
+	HibernateTemplate ht = this.getHibernateTemplate();
+	public abstract List<E> getAll();
+	public abstract E find(E obj);
+	public abstract E findById(Long l);
+	public abstract E findByName(String str);
+	public abstract int delete(E obj);
+	public abstract int sava(E obj);
+	public abstract int update(E obj);
 
 }
