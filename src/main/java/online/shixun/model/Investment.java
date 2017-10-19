7b60196baed4a8ee104af9f5734349fcb7065fa7
jia,bo,ya <1406252302@qq.com>
2017-10-19 11:37:32 +0800

    getset方法

--- src/main/java/online/shixun/model/Investment.java
+++ src/main/java/online/shixun/model/Investment.java
@@ -1,6 +1,5 @@
 package online.shixun.model;
 
-
 import java.sql.Date;
 
 import javax.persistence.Entity;
@@ -10,10 +9,10 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="tb_investment")
+@Table(name = "tb_investment")
 public class Investment {
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long investmentId;
 	private Date beginTime;
 	private Date overTime;
@@ -21,4 +20,61 @@ public class Investment {
 	private int investmentStatus;
 	private int number;
 	private double principal;
+	public long getInvestmentId() {
+		return investmentId;
+	}
+	public void setInvestmentId(long investmentId) {
+		this.investmentId = investmentId;
+	}
+	public Date getBeginTime() {
+		return beginTime;
+	}
+	public void setBeginTime(Date beginTime) {
+		this.beginTime = beginTime;
+	}
+	public Date getOverTime() {
+		return overTime;
+	}
+	public void setOverTime(Date overTime) {
+		this.overTime = overTime;
+	}
+	public double getEarning() {
+		return earning;
+	}
+	public void setEarning(double earning) {
+		this.earning = earning;
+	}
+	public int getInvestmentStatus() {
+		return investmentStatus;
+	}
+	public void setInvestmentStatus(int investmentStatus) {
+		this.investmentStatus = investmentStatus;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public double getPrincipal() {
+		return principal;
+	}
+	public void setPrincipal(double principal) {
+		this.principal = principal;
+	}
+	public Investment(long investmentId, Date beginTime, Date overTime, double earning, int investmentStatus,
+			int number, double principal) {
+		super();
+		this.investmentId = investmentId;
+		this.beginTime = beginTime;
+		this.overTime = overTime;
+		this.earning = earning;
+		this.investmentStatus = investmentStatus;
+		this.number = number;
+		this.principal = principal;
+	}
+	public Investment() {
+		super();
+	}
+	
 }
