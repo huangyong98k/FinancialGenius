2e407ab32a194cb739bbe62711036e77e84f797c
huang,yong <huangyong.nsu@qq.com>
2017-10-19 11:18:49 +0800

    add BaseDao.java

---
+++ src/main/java/online/shixun/dao/BaseDao.java
@@ -0,0 +1,23 @@
+/**   
+* @Title: BaseDao.java 
+* @Package online.shixun.dao 
+* @Description: TODO(用一句话描述该文件做什么) 
+* @author A18ccms A18ccms_gmail_com   
+* @date 2017年10月19日 上午10:56:45 
+* @version V1.0   
+*/
+package online.shixun.dao;
+
+import org.springframework.orm.hibernate4.support.HibernateDaoSupport;
+
+/** 
+* @ClassName: BaseDao 
+* @Description: TODO(这里用一句话描述这个类的作用) 
+* @author HPEU丶小咸鱼
+* @date 2017年10月19日 上午10:56:45 
+*  
+*/
+public abstract class BaseDao extends HibernateDaoSupport{
+	
+
+}
