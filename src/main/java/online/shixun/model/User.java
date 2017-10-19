a35951d4346170c1e734e21b156a0981cd712b7d
huangxinyuan <1019633748@qq.com>
2017-10-19 09:43:36 +0800

    data base added

---
+++ src/main/java/online/shixun/model/User.java
@@ -0,0 +1,33 @@
+package online.shixun.model;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_users")
+public class User {	
+@Id
+@GeneratedValue(strategy=GenerationType.IDENTITY)
+private long userId;
+private String userName;
+@Column(unique=true)
+private String userEmail;
+@Column(unique=true)
+private String userCard;
+private String userPassword;
+private double userBanlance;
+@Column(unique=true)
+private String userPhone;
+private String userStatus;
+@OneToMany
+@JoinColumn(name="userId")
+private List<Investment> investments;
+}
