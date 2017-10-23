a2dd5967905037aa6db51cbf85ebece6186bfb53
huangxinyuan <1019633748@qq.com>
2017-10-23 19:52:08 +0800

    用户删除前后端交互

--- src/main/java/online/shixun/services/UserService.java
+++ src/main/java/online/shixun/services/UserService.java
@@ -46,4 +46,7 @@ public class UserService {
     public List<Investment> findInvestmentsByUserId(Long userId){
 		return userDaoImpl.getInvestmentsByUserId(userId);
 	}
+    public void deleteById(User user) {
+		userDaoImpl.delete(user);
+	}
 }
